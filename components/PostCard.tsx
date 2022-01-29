c098f1ed9e461e09d692b709c87c3969ad3a2ce3
AndersHolt <andhk11@gmail.com>
2022-01-29 18:02:25 +0100

    Making the first step towards the blog

---
+++ components/PostCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Post = {
+    title: string;
+    excerpt: string;
+}
+
+type Props = {
+    post: Post;
+}
+
+const PostCard = ({ post }: Props) => {
+    return (
+        <div>
+            {post.title}
+            {post.excerpt}
+        </div>
+    );
+};
+
+export default PostCard;
